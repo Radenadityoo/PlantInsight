e19d39436f237a50a5f927f9bbfce13175f0cafb
Radenadityoo <cowmangler2000@gmail.com>
2026-07-07 19:40:11 +0700

    Trim Vercel bundle

--- app/model/preprocess.py
+++ app/model/preprocess.py
@@ -1,9 +1,9 @@
 import numpy as np
-from tensorflow.keras.preprocessing import image
+from PIL import Image
 
 def preprocess_image(img_path):
-    img = image.load_img(img_path, target_size=(160, 160))
-    img_array = image.img_to_array(img)
+    img = Image.open(img_path).convert('RGB').resize((160, 160))
+    img_array = np.asarray(img, dtype='float32')
     img_array = img_array.astype('float32') / 255.0
     img_array = np.expand_dims(img_array, axis=0)
     return img_array
